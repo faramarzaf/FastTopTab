466b0215b323d7a0b1a9dcce52fb3cf1142538ad
FaramarzAf <faramarzafzali@gmail.com>
2020-06-07 16:26:44 +0430

    init

---
+++ app/src/main/java/com/faramarz/material/en/fasttoptab/ButtonChooserProfile.java
@@ -0,0 +1,226 @@
+package com.faramarz.material.en.fasttoptab;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+
+public class ButtonChooserProfile extends RelativeLayout implements RadioCheckable {
+    // Views
+    private TextView mValueTextView;
+
+    // Constants
+    public static final int DEFAULT_TEXT_COLOR = Color.TRANSPARENT;
+
+    // Attribute Variables
+    private String mValue;
+    private int mValueTextColor;
+    private int mPressedTextColor;
+
+    // Variables
+    private Drawable mInitialBackgroundDrawable;
+    private OnClickListener mOnClickListener;
+    private OnTouchListener mOnTouchListener;
+    private boolean mChecked;
+    private ArrayList<OnCheckedChangeListener> mOnCheckedChangeListeners = new ArrayList<>();
+
+    //================================================================================
+    // Constructors
+    //================================================================================
+
+    public ButtonChooserProfile(Context context) {
+        super(context);
+        setupView();
+    }
+
+    public ButtonChooserProfile(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        parseAttributes(attrs);
+        setupView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+    public ButtonChooserProfile(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        parseAttributes(attrs);
+        setupView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public ButtonChooserProfile(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        parseAttributes(attrs);
+        setupView();
+    }
+
+    //================================================================================
+    // Init & inflate methods
+    //================================================================================
+
+    private void parseAttributes(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.ButtonChooserProfile, 0, 0);
+        Resources resources = getContext().getResources();
+        try {
+            mValue = a.getString(R.styleable.ButtonChooserProfile_ftt_ButtonValueText);
+            mValueTextColor = a.getColor(R.styleable.ButtonChooserProfile_ftt_ButtonValueTextColor, resources.getColor(R.color.black));
+            mPressedTextColor = a.getColor(R.styleable.ButtonChooserProfile_ftt_ButtonPressedTextColor, Color.WHITE);
+
+        } finally {
+            a.recycle();
+        }
+    }
+
+    // Template method
+    private void setupView() {
+        inflateView();
+        bindView();
+        setCustomTouchListener();
+    }
+
+    protected void inflateView() {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        inflater.inflate(R.layout.custom_preset_button, this, true);
+        mValueTextView = findViewById(R.id.text_view_value);
+        mInitialBackgroundDrawable = getBackground();
+    }
+
+    protected void bindView() {
+        if (mValueTextColor != DEFAULT_TEXT_COLOR) {
+            mValueTextView.setTextColor(mValueTextColor);
+        }
+        mValueTextView.setText(mValue);
+    }
+
+    //================================================================================
+    // Overriding default behavior
+    //================================================================================
+
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+        mOnClickListener = l;
+    }
+
+    protected void setCustomTouchListener() {
+        super.setOnTouchListener(new TouchListener());
+    }
+
+    @Override
+    public void setOnTouchListener(OnTouchListener onTouchListener) {
+        mOnTouchListener = onTouchListener;
+    }
+
+    public OnTouchListener getOnTouchListener() {
+        return mOnTouchListener;
+    }
+
+    private void onTouchDown(MotionEvent motionEvent) {
+        setChecked(true);
+    }
+
+    private void onTouchUp(MotionEvent motionEvent) {
+        // Handle user defined click listeners
+        if (mOnClickListener != null) {
+            mOnClickListener.onClick(this);
+        }
+    }
+    //================================================================================
+    // Public methods
+    //================================================================================
+
+    public void setCheckedState() {
+        setBackgroundResource(R.drawable.background_shape_preset_button__pressed);
+        mValueTextView.setTextColor(mPressedTextColor);
+    }
+
+    public void setNormalState() {
+        setBackgroundDrawable(mInitialBackgroundDrawable);
+        mValueTextView.setTextColor(mValueTextColor);
+
+    }
+
+    public String getValue() {
+        return mValue;
+    }
+
+    public void setValue(String value) {
+        mValue = value;
+    }
+
+
+    //================================================================================
+    // Checkable implementation
+    //================================================================================
+
+    @Override
+    public void setChecked(boolean checked) {
+        if (mChecked != checked) {
+            mChecked = checked;
+            if (!mOnCheckedChangeListeners.isEmpty()) {
+                for (int i = 0; i < mOnCheckedChangeListeners.size(); i++) {
+                    mOnCheckedChangeListeners.get(i).onCheckedChanged(this, mChecked);
+                }
+            }
+            if (mChecked) {
+                setCheckedState();
+            } else {
+                setNormalState();
+            }
+        }
+    }
+
+    @Override
+    public boolean isChecked() {
+        return mChecked;
+    }
+
+    @Override
+    public void toggle() {
+        setChecked(!mChecked);
+    }
+
+    @Override
+    public void addOnCheckChangeListener(OnCheckedChangeListener onCheckedChangeListener) {
+        mOnCheckedChangeListeners.add(onCheckedChangeListener);
+    }
+
+    @Override
+    public void removeOnCheckChangeListener(OnCheckedChangeListener onCheckedChangeListener) {
+        mOnCheckedChangeListeners.remove(onCheckedChangeListener);
+    }
+
+
+    //================================================================================
+    // Inner classes
+    //================================================================================
+    private final class TouchListener implements OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    onTouchDown(event);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    onTouchUp(event);
+                    break;
+            }
+            if (mOnTouchListener != null) {
+                mOnTouchListener.onTouch(v, event);
+            }
+            return true;
+        }
+    }
+}
